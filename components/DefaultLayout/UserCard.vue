3a6f3987655ea7306609912f916d4a4cbaa87703
Ayman Nageeb <aimangta123121@gmail.com>
2024-08-07 10:13:47 +0200

    Refactor: user card UI

--- components/DefaultLayout/UserCard.vue
+++ components/DefaultLayout/UserCard.vue
@@ -2,32 +2,31 @@
   <div class="text-center">
     <v-menu v-model="menuModal.isShown.value" :close-on-content-click="false" location="top">
       <template v-slot:activator="{ props }">
-        <v-avatar color="indigo" v-bind="props" class="cursor-pointer">
-          <v-img :src="user?.avatar"></v-img>
+        <v-avatar size="large" color="indigo" v-bind="props" class="cursor-pointer">
+          <v-img cover :aspect-ratio="1 / 1" :src="user?.avatar"></v-img>
         </v-avatar>
       </template>
 
       <v-card min-width="300" class="pa-4 my-2">
-        <v-list >
-          <v-list-item :prepend-avatar="user?.avatar" :title="user?.name" :subtitle="user?.email">
-
-          </v-list-item>
+        <v-list>
+          <v-list-item :prepend-avatar="user?.avatar" :title="user?.name" :subtitle="user?.email" />
         </v-list>
 
-        <v-divider></v-divider>
+        <!-- <v-divider></v-divider>
 
         <v-list>
-          <!-- <v-list-item>
-                  <v-switch v-model="message" color="purple" label="Enable messages" hide-details></v-switch>
-                </v-list-item>
+          <v-list-item>
+            My Profile
+          </v-list-item>
 
-                <v-list-item>
-                  <v-switch v-model="hints" color="purple" label="Enable hints" hide-details></v-switch>
-                </v-list-item> -->
+          <v-list-item>
+            Settings
+          </v-list-item>
         </v-list>
-        <v-divider></v-divider>
-        <v-card-actions class="pa-2">
-          <v-btn color="error" block @click="doLogout" variant="tonal" prepend-icon="mdi-logout">Logout</v-btn>
+        <v-divider></v-divider> -->
+        <v-card-actions class="px-2 py-4">
+          <v-btn color="error" block @click="doLogout" variant="tonal"
+            prepend-icon="mdi-logout mdi-rotate-180">Logout</v-btn>
         </v-card-actions>
 
       </v-card>
