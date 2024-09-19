d501b21fda506eea5e698b4fdffbc47c048fa0f0
Ayman Nageeb <aimangta123121@gmail.com>
2024-09-19 13:35:10 +0200

    Feature: set new backend

--- app/modules/users/infrastructure/adapters/pocketbase/Connection.ts
+++ app/modules/users/infrastructure/adapters/pocketbase/Connection.ts
@@ -1,3 +1,3 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://pocketbase_8080-ngc04cscsokcokgskgcgw840.153.92.208.30.sslip.io');
+export const pb = new PocketBase('http://pocketbase_8080-ngc04cscsokcokgskgcgw840.153.92.208.30.sslip.io/');
--- composables/pocketbase.ts
+++ composables/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "@/app/pocketbase-types";
 
-const pb = new PocketBase("http://pocketbase_8080-ngc04cscsokcokgskgcgw840.153.92.208.30.sslip.io") as TypedPocketBase;
+const pb = new PocketBase("http://pocketbase_8080-ngc04cscsokcokgskgcgw840.153.92.208.30.sslip.io/") as TypedPocketBase;
 
 pb.autoCancellation(false);
 
